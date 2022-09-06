4cb797057a741fc10dd3803349c2ca4522a05e0b
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-06 22:16:55 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -5,12 +5,13 @@ class Program
 	public static void Main(string[] args)
 	{
 		int num = Convert.ToInt32(Console.ReadLine());
-		if (num > 0){
+		int x = Convert.ToInt32(Console.ReadLine());
+		if (num > x){
 			Console.WriteLine(1);
-		} else if (num == 0){
+		} else if (num == x){
 			Console. WriteLine(0);
 		} else {
-			Console.WriteLine(-1);
+			Console.WriteLine(2);
 		}
 	}
 }
