053ea21d3f4b09888f86086c878af18c63dee4ee
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-06 18:37:24 +0300

    Update NoobCoders.cs

--- NoobCoders.cs
+++ NoobCoders.cs
@@ -4,13 +4,12 @@ class Program
 {
     public static void Main(string[] args)
     {
-        double money = Convert.ToDouble(Console.ReadLine());
-		if (0 < money && money < 100){
-			Console.WriteLine($"Сумма вклада после начисления процентов: {money * 1.05}");
-		} else if (money >= 100 && money <= 200){
-			Console.WriteLine($"Сумма вклада после начисления процентов: {money * 1.07}");
-		} else {
-			Console.WriteLine($"Сумма вклада после начисления процентов: {money * 1.1}");
+        string glasnye = "уеэоаыяиюё";
+		string letter = Console.ReadLine();
+		for (int i = 0; i < glasnye.Length; i++){
+			if (Convert.ToString(letter) == Convert.ToString(glasnye[i])){
+				Console.WriteLine("гласная");
+			}
 		}
 	}
 }
