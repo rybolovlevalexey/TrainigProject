bc2226dce1c0d2b7a1243ad96ee5e62442b5d19c
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-06 18:06:21 +0300

    Update NoobCoders.cs

--- NoobCoders.cs
+++ NoobCoders.cs
@@ -8,13 +8,32 @@ class Program
 		int a = Convert.ToInt32(st[0]);
 		int b = Convert.ToInt32(st[1]);
 		int c = Convert.ToInt32(st[2]);
-
-		if (a <= b && a <= c){
-			Console.WriteLine(a);
-		} else if (b <= a && b <= c){
-			Console.WriteLine(b);
+		int d = Convert.ToInt32(st[3]);
+		int[] ans = new int[2];
+		if (a <= b && a <= c && a <= d){
+			ans[0] = a;
 		} else {
-			Console.WriteLine(c);
+			if (b <= d && b <= a && b <= c){
+				ans[0] = b;
+			} else if (c <= d && c <= b && c <= a){
+				ans[0] = c;
+			} else {
+				ans[0] = d;
+			}
 		}
+
+		if (a >= b && a >= c && a >= d){
+			ans[1] = a;
+		} else {
+			if (b >= d && b >= a && b >= c){
+				ans[1] = b;
+			} else if (c >= d && c >= b && c >= a){
+				ans[1] = c;
+			} else {
+				ans[1] = d;
+			}
+		}
+		Console.WriteLine($"Наименьшее число - {ans[0]}");
+		Console.WriteLine($"Наибольшее число - {ans[1]}");
 	}
 }
