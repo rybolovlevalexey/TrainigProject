e2601278712beac5e825b2ba4ed3542e7669f7fe
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-06 17:36:06 +0300

    Update NoobCoders.cs

--- NoobCoders.cs
+++ NoobCoders.cs
@@ -4,11 +4,42 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Введите два числа через пробел: длина и ширина стола");
-		double a, b;
-		string[] st = Console.ReadLine().Split(" ");
-		a = Convert.ToDouble(st[0]) - 0.2;
-		b = Convert.ToDouble(st[1]) - 0.2;
-		Console.WriteLine($"Площадь стола: {a * b}");
-    }
+        int num;
+		num = Convert.ToInt32(Console.ReadLine());
+		string ans = "";
+		if (num == 0){
+			Console.WriteLine(0);
+		} else {
+			while (num > 0){
+				int ost = num % 16;
+				num = num / 16;
+				//Console.WriteLine(num);
+				if (ost < 10){
+					ans = Convert.ToString(ost) + ans;
+				} else {
+					switch(ost){
+						case 10:
+							ans = "a" + ans;
+							break;
+						case 11:
+							ans = "b" + ans;
+							break;
+						case 12:
+							ans = "c" + ans;
+							break;
+						case 13:
+							ans = "d" + ans;
+							break;
+						case 14:
+							ans = "e" + ans;
+							break;
+						case 15:
+							ans = "f" + ans;
+							break;
+					}
+				}
+			}
+			Console.WriteLine(ans);
+	    }
+	}
 }
