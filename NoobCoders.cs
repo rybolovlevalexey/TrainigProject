c23af2a66fb6e627839d97f3e59bc04b1f84d1f5
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-06 18:11:45 +0300

    Update NoobCoders.cs

--- NoobCoders.cs
+++ NoobCoders.cs
@@ -4,36 +4,13 @@ class Program
 {
     public static void Main(string[] args)
     {
-        string[] st = Console.ReadLine().Split();
-		int a = Convert.ToInt32(st[0]);
-		int b = Convert.ToInt32(st[1]);
-		int c = Convert.ToInt32(st[2]);
-		int d = Convert.ToInt32(st[3]);
-		int[] ans = new int[2];
-		if (a <= b && a <= c && a <= d){
-			ans[0] = a;
+        double money = Convert.ToDouble(Console.ReadLine());
+		if (0 < money && money < 100){
+			Console.WriteLine($"Сумма вклада после начисления процентов: {money * 1.05}");
+		} else if (money >= 100 && money <= 200){
+			Console.WriteLine($"Сумма вклада после начисления процентов: {money * 1.07}");
 		} else {
-			if (b <= d && b <= a && b <= c){
-				ans[0] = b;
-			} else if (c <= d && c <= b && c <= a){
-				ans[0] = c;
-			} else {
-				ans[0] = d;
-			}
+			Console.WriteLine($"Сумма вклада после начисления процентов: {money * 1.1}");
 		}
-
-		if (a >= b && a >= c && a >= d){
-			ans[1] = a;
-		} else {
-			if (b >= d && b >= a && b >= c){
-				ans[1] = b;
-			} else if (c >= d && c >= b && c >= a){
-				ans[1] = c;
-			} else {
-				ans[1] = d;
-			}
-		}
-		Console.WriteLine($"Наименьшее число - {ans[0]}");
-		Console.WriteLine($"Наибольшее число - {ans[1]}");
 	}
 }
