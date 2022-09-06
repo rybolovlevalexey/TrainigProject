7f885804817123cb196c88179d248302cbbcd0f0
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-06 17:07:54 +0300

    Update NoobCoders.cs

--- NoobCoders.cs
+++ NoobCoders.cs
@@ -1,13 +1,14 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-      Console.WriteLine ("Введите значение радиуса круга");
-      double r;
-      r = Convert.ToDouble(Console.ReadLine());
-      double p, s;
-      p = 2 * r * Math.PI;
-      s = Math.PI * r * r;
-      Console.WriteLine($"Периметр: {p}, площадь: {s}");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите два числа через пробел: длина и ширина стола");
+		double a, b;
+		string[] st = Console.ReadLine().Split(" ");
+		a = Convert.ToDouble(st[0]) - 0.2;
+		b = Convert.ToDouble(st[1]) - 0.2;
+		Console.WriteLine($"Площадь стола: {a * b}");
+    }
 }
