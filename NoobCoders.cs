6175a531588d93dae90b615da1d3dcf7e6a8daa1
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-06 17:03:06 +0300

    Update NoobCoders.cs

--- NoobCoders.cs
+++ NoobCoders.cs
@@ -1,1 +1,13 @@
+using System;
 
+class Program {
+  public static void Main (string[] args) {
+      Console.WriteLine ("Введите значение радиуса круга");
+      double r;
+      r = Convert.ToDouble(Console.ReadLine());
+      double p, s;
+      p = 2 * r * Math.PI;
+      s = Math.PI * r * r;
+      Console.WriteLine($"Периметр: {p}, площадь: {s}");
+  }
+}
