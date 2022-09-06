bea27c89acb0f32de88d18504438107842f2b03d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-09-06 07:34:59 +0300

    Задача 1 из практикума
    Signed-off-by: Rybolovlev Alexey <rybolovlevalexey@yandex.ru>

---
+++ ProgrammingPracticum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProgrammingPracticum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n, k;
+            n = Convert.ToInt32(Console.ReadLine());
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((n - k % n) % n);
+        }
+    }
+}
