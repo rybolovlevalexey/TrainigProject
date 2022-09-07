7853cadc463d529686596d4f5c8f57616c4471b9
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-07 08:21:26 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -5,15 +5,19 @@ class Program
 	public static void Main(string[] args)
 	{
 		int num = Convert.ToInt32(Console.ReadLine());
-		int ans = 0;
-		while (num != 1){
-			if (num % 2 == 0){
-				num /= 2;
-			} else {
-				num = num * 3 + 1;
+		int mxcnt = num * 2 - 1;
+		string st = "";
+		int i = 1;
+		while (i <= num){
+			st = "";
+			for (int j = 0; j < num - i; j++){
+				st = st + " ";
 			}
-			ans++;
+			for (int j = 0; j < i * 2 - 1; j++){
+				st = st + "*";
+			}
+			Console.WriteLine(st);
+			i++;
 		}
-		Console.WriteLine($"Количество необходимых действий над числом - {ans}");
 	}
 }
