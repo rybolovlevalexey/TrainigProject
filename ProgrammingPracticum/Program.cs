4b256d387fbd1d1b107a8676d2c51ebe3599083f
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-07 11:33:38 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -4,35 +4,13 @@ class Program
 {
 	public static void Main(string[] args)
 	{
-		string st1 = Console.ReadLine();
-		string st2 = Console.ReadLine();
-		string ans = "";
-		string newst1 = "";
-		for (int i = 0; i < st1.Length; i++){
-			bool flag = true;
-			for (int j = 0; j < st1.Length; j++){
-				if (i == j){
-					continue;
-				}
-				if (st1[i] == st1[j]){
-					flag = false;
-				}
-			}
-			if (flag){
-				newst1 = newst1 + st1[i];
+		string[] st = Console.ReadLine().Split();
+		int summa = 0;
+		for (int i = 0; i < st.Length; i++){
+			if (Convert.ToInt32(st[i]) % 2 == 0){
+				summa += Convert.ToInt32(st[i]);
 			}
 		}
-		foreach (var letter in newst1){
-			bool flag1 = true;
-			foreach (var bykv in st2){
-				if (letter == bykv){
-					flag1 = false;
-				}
-			}
-			if (flag1){
-				ans = ans + Convert.ToString(letter) + " ";
-			}
-		}
-		Console.WriteLine($"Уникально число {ans}");
+		Console.WriteLine(summa);
 	}
 }
