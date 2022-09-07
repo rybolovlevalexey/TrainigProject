f21a1237b479fe5ef6a10d2f6327dce93e530bd7
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-07 08:02:17 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -5,10 +5,24 @@ class Program
 	public static void Main(string[] args)
 	{
 		int n = Convert.ToInt32(Console.ReadLine());
-		int fak = 1;
-		for (int i = 1; i <= n; i++){
-			fak *= i;
+		int m = Convert.ToInt32(Console.ReadLine());
+		int summa = 0;
+		for(int i = n; i <= m; i++){
+			bool flag = true;
+			if (i == 1){
+				flag = false;
+			} else {
+				for (int d = 2; d < i/2+1; d++){
+					if (i % d == 0 && i != d){
+						flag = false;
+						break;
+					}
+				}
+			}
+			if (flag && i > 0){
+				summa += i;
+			}
 		}
-		Console.WriteLine(fak);
+		Console.WriteLine($"Сумма простых чисел = {summa}");
 	}
 }
