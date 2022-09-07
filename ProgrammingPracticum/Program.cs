0ca8e0d7e1728ed09b5a5b21839b6b080eb9acb5
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-07 11:48:19 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -5,12 +5,25 @@ class Program
 	public static void Main(string[] args)
 	{
 		string[] st = Console.ReadLine().Split();
-		int summa = 0;
+		int[] numbers = new int[st.Length];
 		for (int i = 0; i < st.Length; i++){
-			if (Convert.ToInt32(st[i]) % 2 == 0){
-				summa += Convert.ToInt32(st[i]);
+			numbers[i] = Convert.ToInt32(st[i]);
+		}
+		int mn = 0, mx = 0;
+		for (int i = 0; i < numbers.Length; i++){
+			if (i == 0){
+				mn = numbers[i];
+				mx = numbers[i];
+			} else {
+				if (numbers[i] < mn){
+					mn = numbers[i];
+				}
+				if (numbers[i] > mx){
+					mx = numbers[i];
+				}
 			}
 		}
-		Console.WriteLine(summa);
+		Console.WriteLine($"Наименьшее значение = {mn}");
+		Console.WriteLine($"Наибольшее значение = {mx}");
 	}
 }
