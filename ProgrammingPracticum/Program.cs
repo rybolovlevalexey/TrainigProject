91b1c3aa23e27d4dc90db37bf5e4c794924e46ed
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-07 07:56:06 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -5,15 +5,10 @@ class Program
 	public static void Main(string[] args)
 	{
 		int n = Convert.ToInt32(Console.ReadLine());
-		int summa = 0;
-		int i = 0;
-		while (true){
-			if (summa + i >= n){
-				break;
-			}
-			Console.WriteLine(i);
-			summa += i;
-			i++;
+		int fak = 1;
+		for (int i = 1; i <= n; i++){
+			fak *= i;
 		}
+		Console.WriteLine(fak);
 	}
 }
