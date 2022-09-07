0fcac0429687e294b323096e6104c2be65622e72
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-07 21:43:49 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -9,9 +9,17 @@ class Program
 		double x2 = Convert.ToDouble(Console.ReadLine());
 		double y2 = Convert.ToDouble(Console.ReadLine());
 		bool flag = false;
-		int[] numbers = {-1, 0, 1};
-		foreach (var i in numbers){
-			foreach (var j in numbers){
+		int[] numb1 = {-1, 1};
+		int[] numb2 = {-2, 2};
+		foreach (var i in numb1){
+			foreach (var j in numb2){
+				if (x1 + i == x2 && y1 + j == y2){
+					flag = true;
+				}
+			}
+		}
+		foreach (var i in numb2){
+			foreach (var j in numb1){
 				if (x1 + i == x2 && y1 + j == y2){
 					flag = true;
 				}
