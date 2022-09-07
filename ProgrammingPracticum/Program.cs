1a8b6896218ec35a78e806a80d81e5f872e02631
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-07 12:04:19 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -4,23 +4,32 @@ class Program
 {
 	public static void Main(string[] args)
 	{
-		string[] st = Console.ReadLine().Split();
+		string[] st = Console.ReadLine().Split(", ");
 		int[] numbers = new int[st.Length];
+		int summa = 0;
 		for (int i = 0; i < st.Length; i++){
 			numbers[i] = Convert.ToInt32(st[i]);
+			summa += Convert.ToInt32(st[i]);
 		}
-		for (int i = 0; i < numbers.Length; i++){
-			for (int j = i; j < numbers.Length - 1; j++){
-				int a = numbers[j];
-				int b = numbers[j + 1];
-				if (a > b){
-					numbers[j] = b;
-					numbers[j + 1] = a;
+		if (summa == 20){
+			Console.WriteLine("О, отличник появился! На олимпиаду пойдешь");
+		} else {
+			bool flag2 = false, flag3 = false;
+			foreach (var num in numbers){
+				if (num == 2){
+					flag2 = true;
+				}
+				if (num == 3){
+					flag3 = true;
 				}
 			}
-		}
-		foreach (var num in numbers){
-			Console.Write($"{num} ");
+			if (flag2){
+				Console.WriteLine("Ну что, студент, пора долг Родине отдать");
+			} else if (flag3){
+				Console.WriteLine("Прощай стипендия!");
+			} else {
+				Console.WriteLine("Живи пока, через полгода увидимся");
+			}
 		}
 	}
 }
