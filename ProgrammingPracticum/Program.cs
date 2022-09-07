ac7a01f04a9d224e4e534f50fa26eb5e20f15aef
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-07 08:15:04 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -4,29 +4,16 @@ class Program
 {
 	public static void Main(string[] args)
 	{
-		int first = Convert.ToInt32(Console.ReadLine());
-		int second = Convert.ToInt32(Console.ReadLine());
-		int n = first, m = second;
-		int ans = -1;
-		while (n > 0){
-			m = second;
-			while (m > 0){
-				if (m % 10 == n % 10){
-					ans = m % 10;
-					break;
-				}
-				m = m / 10;
+		int num = Convert.ToInt32(Console.ReadLine());
+		int ans = 0;
+		while (num != 1){
+			if (num % 2 == 0){
+				num /= 2;
+			} else {
+				num = num * 3 + 1;
 			}
-			n = n / 10;
-			if (ans != - 1){
-				break;
-			}
+			ans++;
 		}
-		while (first > 0){
-			if (first % 10 == ans){
-				Console.Write($"{ans} ");
-			}
-			first = first / 10;
-		}
+		Console.WriteLine($"Количество необходимых действий над числом - {ans}");
 	}
 }
