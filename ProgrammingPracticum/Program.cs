c4e2d11f0e8902ab1b5dd092c7e020bd4480f5de
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-08 09:28:30 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -4,31 +4,23 @@ class Program
 {
 	public static void Main(string[] args)
 	{
-		double x1 = Convert.ToDouble(Console.ReadLine());
-		double y1 = Convert.ToDouble(Console.ReadLine());
-		double x2 = Convert.ToDouble(Console.ReadLine());
-		double y2 = Convert.ToDouble(Console.ReadLine());
-		bool flag = false;
-		int[] numb1 = {-1, 1};
-		int[] numb2 = {-2, 2};
-		foreach (var i in numb1){
-			foreach (var j in numb2){
-				if (x1 + i == x2 && y1 + j == y2){
-					flag = true;
+		int n = Convert.ToInt32(Console.ReadLine());
+		int summa = 0;
+		int mx = -1, mn = -1;
+		while (n > 0){
+			if (mx == -1){
+				mx = n % 10;
+				mn = n % 10;
+			} else {
+				if (mx < n % 10){
+					mx = n % 10;
+				}
+				if (mn > n % 10){
+					mn = n % 10;
 				}
 			}
+			n /= 10;
 		}
-		foreach (var i in numb2){
-			foreach (var j in numb1){
-				if (x1 + i == x2 && y1 + j == y2){
-					flag = true;
-				}
-			}
-		}
-		if (flag){
-			Console.WriteLine("YES");
-		} else {
-			Console.WriteLine("NO");
-		}
+		Console.WriteLine($"{mn} {mx}");
 	}
 }
