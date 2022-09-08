da2f43da97f63bb2e910e91e8d981ce3e1b0246b
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-08 20:51:39 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -4,20 +4,14 @@ class Program
 {
 	public static void Main(string[] args)
 	{
-		int n = Convert.ToInt32(Console.ReadLine());
-		int cnt = 0, num = 1;
-		while (num <= n){
-			string st1 = Convert.ToString(num), st2 = "";
-			int rnum = num;
-			while (rnum > 0){
-				st2 = st2 + Convert.ToString(rnum % 10);
-				rnum /= 10;
-			}
-			num += 1;
-			if (st1 == st2){
-				cnt += 1;
-			}
+		int n, m;
+		string[] st = Console.ReadLine().Split();
+		n = Convert.ToInt32(st[0]);
+		m = Convert.ToInt32(st[1]);
+		if (m % n == 0){
+			Console.WriteLine(m / n);
+		} else {
+			Console.WriteLine(m / n + 1);
 		}
-		Console.WriteLine(cnt);
 	}
 }
