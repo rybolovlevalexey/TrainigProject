d2284e7c239924acb9b5efd93475d85b68ab10af
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-10 18:21:38 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -10,10 +10,10 @@ class Program
 		int cnt = 0;
 		bool flag = false;
 		ans[0] = spisok[n - 1];
-		for (int i = 1; i + 1 < n; i++){
+		for (int i = 0; i + 1 < n; i++){
 			ans[i + 1] = spisok[i];
 		}
-		foreach (var el in spisok){
+		foreach (var el in ans){
 			Console.Write($"{el} ");
 		}
 	}
