0ea5c5a978ac71588bfe8006cbb4ce035b7df50c
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-10 17:49:56 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -7,12 +7,12 @@ class Program
 		int n = Convert.ToInt32(Console.ReadLine());
 		string[] spisok = Console.ReadLine().Split();
 		int cnt = 0;
-		for (int i = 0; i < n; i++){
-			Console.WriteLine(Convert.ToInt32(spisok[i]));
-			if (Convert.ToInt32(spisok[i]) > 0){
-				cnt += 1;	
-			} 
-			
+		for (int i = 0; i + 1 < n; i++){
+			int a = Convert.ToInt32(spisok[i]);
+			int b = Convert.ToInt32(spisok[i + 1]);
+			if (a < b){
+				cnt += 1;
+			}
 		}
 		Console.WriteLine(cnt);
 	}
