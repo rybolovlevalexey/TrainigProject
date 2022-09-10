df9af565c88034fc3f7ab6635414e133ab8aaae7
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-10 20:45:43 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -4,21 +4,34 @@ class Program
 {
 	public static void Main(string[] args)
 	{
-		int n = Convert.ToInt32(Console.ReadLine());
-		string[] sp = Console.ReadLine().Split();
-		int lst = 0;
-		int ans = 0;
-		for (int i = 0; i < n; i++){
-			if (i == 0){
-				ans += 1;
-				lst = Convert.ToInt32(sp[i]);
-			} else {
-				if (lst != Convert.ToInt32(sp[i])){
-					ans += 1;
-					lst = Convert.ToInt32(sp[i]);
-				}
-			}
+		int n, a, b, c, d;
+		string[] st = Console.ReadLine().Split();
+		n = Convert.ToInt32(st[0]);
+		a = Convert.ToInt32(st[1]) - 1;
+		b = Convert.ToInt32(st[2]) - 1;
+		c = Convert.ToInt32(st[3]) - 1;
+		d = Convert.ToInt32(st[4]) - 1;
+		int[] sp = new int[n];
+		for (int k = 0; k < n; k++){
+			sp[k] = k + 1;
 		}
-		Console.WriteLine(ans);
+		int i = 0;
+		while (a + 2 * i <= b){
+			int x1 = sp[a + i], x2 = sp[b - i];
+			sp[a + i] = x2;
+			sp[b - i] = x1;
+			i += 1;
+		}
+		i = 0;
+		while (c + i * 2 <= d){
+			int y1 = sp[c + i];
+			int y2 = sp[d - i];
+			sp[c + i] = y2;
+			sp[d - i] = y1;
+			i += 1;
+		}
+		foreach (var elem in sp){
+			Console.Write($"{elem} ");
+		}
 	}
 }
