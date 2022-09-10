c2f757be1a44b4b1954022f23b8f15ed1f3c2474
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-10 17:53:09 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -7,13 +7,19 @@ class Program
 		int n = Convert.ToInt32(Console.ReadLine());
 		string[] spisok = Console.ReadLine().Split();
 		int cnt = 0;
+		bool flag = false;
 		for (int i = 0; i + 1 < n; i++){
-			int a = Convert.ToInt32(spisok[i]);
-			int b = Convert.ToInt32(spisok[i + 1]);
-			if (a < b){
-				cnt += 1;
+			char a = spisok[i][0];
+			char b = spisok[i + 1][0];
+			if (a == b && b == '-'){
+				flag = true;
 			}
 		}
-		Console.WriteLine(cnt);
+		if (flag){
+			Console.WriteLine("YES");
+		} else {
+			Console.WriteLine("NO");
+
+		}
 	}
 }
