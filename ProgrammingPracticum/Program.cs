0fc408747a381bfa960504f75cd0cdc421939d5a
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-11 14:47:43 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -6,24 +6,42 @@ class Program
 	public static void Main(string[] args)
 	{
 		int n = Convert.ToInt32(Console.ReadLine());
-		string[] st = Console.ReadLine().Split();
-		List<int> sp = new List<int>();
-		for (int i = 0; i < n; i++){
-			sp.Add(Convert.ToInt32(st[i]));
-		}
-		sp.Sort();
-		int ans = 0, lst = -1;
-		for (int i = 0; i < n; i++){
-			if (i == 0){
-				ans += 1;
-				lst = sp[i];
-			} else {
-				if (sp[i] != lst){
-					lst = sp[i];
-					ans += 1;
+		List<int> spisok = new List<int>();
+		int dlina = 0;
+		for (int k = 0; k < n; k++){
+			string[] st = Console.ReadLine().Split();
+			if (st[0] == "ADD"){
+				int num = Convert.ToInt32(st[1]);
+				bool flag = true;
+				foreach (var elem in spisok){
+					if (elem == num){
+						flag = false;
+					}
+				}
+				if (flag){
+					spisok.Add(num);
+					dlina += 1;
+				}	
+			}
+			
+			if (st[0] == "COUNT"){
+				Console.WriteLine(dlina);
+			}
+
+			if (st[0] == "PRESENT"){
+				int num = Convert.ToInt32(st[1]);
+				bool flag = false;
+				foreach (var elem in spisok){
+					if (num == elem){
+						flag = true;
+					}
+				}
+				if (flag){
+					Console.WriteLine("YES");
+				} else {
+					Console.WriteLine("NO");
 				}
 			}
 		}
-		Console.WriteLine(ans);
 	}
 }
