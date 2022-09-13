618d280b1480d5cf7417173c219f764b60d54714
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-13 12:14:03 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -5,16 +5,17 @@ class Program
 {
 	public static void Main(string[] args)
 	{
-		int num = 0, last = 999, ans = 0;
+		int num = 0, last = 999999, ans = 0;
 		while (num <= last){
+			bool flag = true;
 			string st = Convert.ToString(num);
-			int cnt = 0;
-			foreach (var elem in st){
-				if (elem == '7'){
-					cnt += 1;
+			for (int i = 0; i < st.Length - 1; i++){
+				if (st[i] == st[i + 1]){
+					flag = false;
+					break;
 				}
 			}
-			if (cnt == 1){
+			if (flag){
 				ans += 1;
 			}
 			num += 1;
