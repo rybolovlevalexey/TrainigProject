111331efe65bc044e7643d4feb6ed20576216e33
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-16 16:02:36 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -1,25 +1,63 @@
 using System;
-using System.Collections.Generic;
 
-class Program
+namespace Консольное_приложение
 {
-	public static void Main(string[] args)
-	{
-		int num = 0, last = 999999, ans = 0;
-		while (num <= last){
-			bool flag = true;
-			string st = Convert.ToString(num);
-			for (int i = 0; i < st.Length - 1; i++){
-				if (st[i] == st[i + 1]){
-					flag = false;
-					break;
-				}
-			}
-			if (flag){
-				ans += 1;
-			}
-			num += 1;
-		}
-		Console.WriteLine(ans);
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string st = Console.ReadLine();
+            string ans_st = "";
+            if (st.Length <= 1)
+            {
+                ans_st = st;
+            }
+            if (st.Length < 4 && st.Length > 1)
+            {
+                string first = "", last = "";
+                int i = 0;
+                foreach (var elem in st)
+                {
+                   if (i == 0)
+                    {
+                        first = Convert.ToString(elem);
+                    }
+                   if (i == st.Length - 1)
+                    {
+                        last = Convert.ToString(elem);
+                    }
+                   if (i != st.Length - 1 && i != 0)
+                    {
+                        ans_st = ans_st + Convert.ToString(elem);
+                    }
+                    i += 1;
+                }
+                ans_st = last + ans_st + first;
+            }
+            if (st.Length >= 4)
+            {
+                string first = "", fourth = "";
+                int i = 0;
+                foreach (var elem in st)
+                {
+                    if (i == 0)
+                    {
+                        first = Convert.ToString(elem);
+                    }
+                    if (i == 3)
+                    {
+                        fourth = Convert.ToString(elem);
+                        ans_st += first;
+                    }
+                    if (i != 0 && i != 3)
+                    {
+                        ans_st += Convert.ToString(elem);
+                    }
+                    i += 1;
+                }
+                ans_st = fourth + ans_st;
+            }
+            Console.WriteLine(ans_st);
+        }
+    }
 }
