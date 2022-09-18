102dc38318c10fdd50e2ec765bfbb602445afe8f
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-18 17:03:55 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -7,28 +7,15 @@ namespace Консольное_приложение
     {
         static void Main(string[] args)
         {
-            List<int> sp = new List<int>();
-            int num = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i <= num / 2 + 1; i += 1) {
-                bool flag = true;
-                if (num % i == 0)
+            string st = Console.ReadLine();
+            int i = 0;
+            foreach (var elem in st)
+            {
+                if (Convert.ToString(elem) == "O")
                 {
-                    int d = num / i;
-                    foreach (var elem in sp)
-                    {
-                        if (elem == i || elem == d)
-                        {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag)
-                    {
-                        Console.WriteLine($"{num} = {i} * {d}");
-                        sp.Add(i);
-                        sp.Add(d);
-                    }
+                    Console.WriteLine(i);
                 }
+                i += 1;
             }
         }
     }
