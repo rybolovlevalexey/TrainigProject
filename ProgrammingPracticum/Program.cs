ab374ee143deebde0c37064d7fe179731db8c712
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-20 18:08:30 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -7,47 +7,55 @@ namespace Консольное_приложение
     {
         static void Main(string[] args)
         {
-            string st = Console.ReadLine();
-            Dictionary<char, List<int>> letters = new Dictionary<char, List<int>>();
-            int ind = 0;
-            foreach (var elem in st)
+            int n = Convert.ToInt32(Console.ReadLine());
+            Dictionary<int, List<int>> all_dots = new Dictionary<int, List<int>>();
+            for (int i = 0; i < n; i += 1)
             {
-                bool flag = true;
-                foreach (var key in letters.Keys)
+                string[] st = Console.ReadLine().Split();
+                int x = Convert.ToInt32(st[0]);
+                int y = Convert.ToInt32(st[1]);
+                List<int> xy = new List<int>();
+                xy.Add(x);
+                xy.Add(y);
+                all_dots[i] = xy;
+            }
+            double radius = 0;
+            for (int i = 0; i < n - 1; i += 1)
+            {
+                for (int j = i + 1; j < n; j += 1)
                 {
-                    if (key == elem)
+                    int dx = all_dots[i][0] - all_dots[j][0];
+                    int dy = all_dots[i][1] - all_dots[j][1];
+                    double dist = (Math.Pow(Math.Pow(dx, 2) + Math.Pow(dy, 2), 0.5));
+                    string stdist = Convert.ToString(dist);
+                    string ansst = "";
+                    int cnt_numbs = 0;
+                    bool comma_flag = false;
+                    foreach (var el in stdist)
                     {
-                        flag = false;
+                        if (el == ',')
+                        {
+                            comma_flag = true;
+                            ansst += el;
+                        }
+                        if (!comma_flag)
+                        {
+                            ansst += el;
+                        }
+                        if (comma_flag && cnt_numbs < 10 && el != ',')
+                        {
+                            cnt_numbs += 1;
+                            ansst += el;
+                        }
+                    }
+                    double ans = Convert.ToDouble(ansst);
+                    if (radius == 0 || ans > radius)
+                    {
+                        radius = ans;
                     }
                 }
-                if (flag)
-                {
-                    List<int> sp = new List<int>();
-                    sp.Add(ind);
-                    letters[elem] = sp;
-                } else
-                {
-                    List<int> sp = new List<int>();
-                    sp = letters[elem];
-                    sp.Add(ind);
-                    letters[elem] = sp;
-                }
-                ind += 1;
             }
-            List<char> keys = new List<char>();
-            foreach (var el in letters.Keys)
-            {
-                keys.Add(el);
-            }
-            keys.Sort();
-            foreach (var key in keys)
-            {
-                Console.Write($"{key} ");
-                foreach (var index in letters[key])
-                {
-                    Console.Write($"{index} ");
-                }
-            }
+            Console.WriteLine(radius);
         }
     }
 }
