f38967178af9c9b7275226a691cc6128a9075aac
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-09-23 16:25:31 +0300

    Update Program.cs

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -7,55 +7,23 @@ namespace Консольное_приложение
     {
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-            Dictionary<int, List<int>> all_dots = new Dictionary<int, List<int>>();
-            for (int i = 0; i < n; i += 1)
+            char st = Convert.ToChar(Console.ReadLine());
+            string num = "0123456789";
+            bool flag = false;
+            foreach (var el in num)
             {
-                string[] st = Console.ReadLine().Split();
-                int x = Convert.ToInt32(st[0]);
-                int y = Convert.ToInt32(st[1]);
-                List<int> xy = new List<int>();
-                xy.Add(x);
-                xy.Add(y);
-                all_dots[i] = xy;
-            }
-            double radius = 0;
-            for (int i = 0; i < n - 1; i += 1)
-            {
-                for (int j = i + 1; j < n; j += 1)
+                if (el == st)
                 {
-                    int dx = all_dots[i][0] - all_dots[j][0];
-                    int dy = all_dots[i][1] - all_dots[j][1];
-                    double dist = (Math.Pow(Math.Pow(dx, 2) + Math.Pow(dy, 2), 0.5));
-                    string stdist = Convert.ToString(dist);
-                    string ansst = "";
-                    int cnt_numbs = 0;
-                    bool comma_flag = false;
-                    foreach (var el in stdist)
-                    {
-                        if (el == ',')
-                        {
-                            comma_flag = true;
-                            ansst += el;
-                        }
-                        if (!comma_flag)
-                        {
-                            ansst += el;
-                        }
-                        if (comma_flag && cnt_numbs < 10 && el != ',')
-                        {
-                            cnt_numbs += 1;
-                            ansst += el;
-                        }
-                    }
-                    double ans = Convert.ToDouble(ansst);
-                    if (radius == 0 || ans > radius)
-                    {
-                        radius = ans;
-                    }
+                    flag = true;
                 }
             }
-            Console.WriteLine(radius);
+            if (flag)
+            {
+                Console.WriteLine("yes");
+            } else
+            {
+                Console.WriteLine("no");
+            }
         }
     }
 }
