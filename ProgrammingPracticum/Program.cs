26b4471d8b0409aecbd733e52ca7b339215b1f79
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-10-17 14:09:56 +0300

    Создание матрицы и вычисление её определителя

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -1,187 +1,65 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Diagnostics;
 
-namespace Быстрое_возведение_в_степень__Сортировки__Бинарный_поиск
+namespace Консольное_приложение
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //FastStepen(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
-
-            //Sortirovky();
-
-            //Console.WriteLine(BinarySearch(Convert.ToInt32(Console.ReadLine())));
-        }
-
-        static int BinarySearch(int number)
-        {
-            int left = 0, rigth = number, mid = -1;
-            while (rigth - left > 1)
+            int n = Convert.ToInt32(Console.ReadLine());  // размеры матрицы
+            // создание матрицы из случайных чисел
+            int[,] matrix = new int[n, n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i += 1)
             {
-                mid = (rigth + left) / 2;
-                if (mid * mid == number)
+                for (int j = 0; j < n; j += 1)
                 {
-                    return mid;
+                    int value = rnd.Next(n*n);
+                    matrix[i, j] = value;
+                    Console.Write($"{value} ");
                 }
-                if (mid * mid < number)
-                {
-                    left = mid;
-                } else
-                {
-                    rigth = mid;
-                }
-                //Console.WriteLine($"{left}, {rigth}");
+                Console.WriteLine();
             }
-            if (mid * mid == number)
-            {
-                return mid;
-            }
-            if (left * left == number)
-            {
-                return left;
-            }
-            if (rigth * rigth == number)
-            {
-                return rigth;
-            }
-            return -1;
-        }
+            // вычисление определителя
 
-        static void Sortirovky()
-        {
-            List<int> sp = new List<int>();
-            try
-            {
-                string strLine;
-                StreamReader file = File.OpenText("C:/Visual Studio Projects/data.txt");    
-                while (null != (strLine = file.ReadLine()))
+            static int Determinant(int[,] sp){
+                if (Convert.ToInt32(Math.Pow(sp.Length, 0.5)) == 1)
                 {
-                    sp.Add(Convert.ToInt32(strLine));
+                    return (sp[0, 0]);
                 }
-            } catch (FileNotFoundException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            int[] sp1 = new int[sp.Count];
-            int[] sp2 = new int[sp.Count];
-            for (int i = 0; i < sp.Count; i += 1)
-            {
-                sp1[i] = sp[i];
-                sp2[i] = sp[i];
-            }
-            
-            Stopwatch stopwatch1 = new Stopwatch();
-            stopwatch1.Start(); // запуск секундомера первой сортировки
-            foreach (var elem in sp1)
-            {
-                Console.Write($"{elem} ");
-            }
-            Console.WriteLine();
-            for (int i = 0; i < sp1.Length; i += 1)
-            {
-                for (int j = 0; j < sp1.Length - 1; j += 1)
+                if (Convert.ToInt32(Math.Pow(sp.Length, 0.5)) == 2)
                 {
-                    if (sp1[j] > sp1[j + 1])
-                    {
-                        int a = sp1[j], b = sp1[j + 1];
-                        sp1[j] = b;
-                        sp1[j + 1] = a;
-                    }
+                    return (sp[0, 0] * sp[1, 1] - sp[0, 1] * sp[1, 0]);
                 }
-            }
-            foreach (var elem in sp1)
-            {
-                Console.Write($"{elem} ");
-            }
-            Console.WriteLine();
-            stopwatch1.Stop();  // сортировка выполнена, секундомер остановлен
-            Console.WriteLine($"Time: {stopwatch1.ElapsedMilliseconds}");
-            
-            Stopwatch stopwatch2 = new Stopwatch();
-            stopwatch2.Start();   // старт второй сортировки
-            static int[] MergeSort(int[] spisok)
-            {
-                if (spisok.Length > 1)
+                int ans = 0;
+                for (int i = 0; i < Convert.ToInt32(Math.Pow(sp.Length, 0.5)); i += 1)
                 {
-                    int mid = spisok.Length / 2;
-                    int[] left = new int[mid];
-                    int[] rigth = new int[spisok.Length - mid];
-                    for (int ind = 0; ind < spisok.Length; ind += 1)
-                    {
-                        if (ind < mid)
+                    int[,] sp1 = new int[Convert.ToInt32(Math.Pow(sp.Length, 0.5)) - 1, Convert.ToInt32(Math.Pow(sp.Length, 0.5)) - 1];
+                    //Console.WriteLine(sp.Length);
+                    for (int j = 1; j < Convert.ToInt32(Math.Pow(sp.Length, 0.5)); j += 1) { 
+                        for (int k = 0; k < Convert.ToInt32(Math.Pow(sp.Length, 0.5)); k += 1)
                         {
-                            left[ind] = spisok[ind];
-                        } else
-                        {
-                            rigth[ind - mid] = spisok[ind];
+                            if (i == k)
+                            {
+                                continue;
+                            }
+                            if (i > k)
+                            {
+                                sp1[j - 1, k] = sp[j, k];
+                            } else
+                            {
+                                sp1[j - 1, k - 1] = sp[j, k];
+                            }
+                            
                         }
                     }
-                    MergeSort(left);
-                    MergeSort(rigth);
-                    int i = 0, j = 0, k = 0;
+                    ans += Convert.ToInt32(Math.Pow((-1), i)) * sp[0, i] * Determinant(sp1);
+                }
+                return ans;
+            }
 
-                    while (i < left.Length && j < rigth.Length)
-                    {
-                        if (left[i] < rigth[j])
-                        {
-                            spisok[k] = left[i];
-                            i += 1;
-                        } else
-                        {
-                            spisok[k] = rigth[j];
-                            j += 1;
-                        }
-                        k += 1;
-                    }
-                    while (i < left.Length)
-                    {
-                        spisok[k] = left[i];
-                        i += 1;
-                        k += 1;
-                    }
-                    while (j < rigth.Length)
-                    {
-                        spisok[k] = rigth[j];
-                        j += 1;
-                        k += 1;
-                    }
-                }
-                return spisok;
-            }
-            foreach (var elem in sp2)
-            {
-                Console.Write($"{elem} ");
-            }
-            sp2 = MergeSort(sp2);
-            Console.WriteLine();
-            foreach (var elem in sp2)
-            {
-                Console.Write($"{elem} ");
-            }
-            stopwatch2.Stop();  // конец второй сортировки
-            Console.WriteLine($"Time: {stopwatch2.ElapsedMilliseconds}");
-        }
-
-        static int FastStepen(int num, int step)
-        {
-            if (step == 1)
-            {
-                return num;
-            }
-            else
-            {
-                if (step % 2 == 1)
-                {
-                    return num * FastStepen(num, step - 1);
-                }
-                else
-                {
-                    return FastStepen(num, step / 2) * FastStepen(num, step / 2);
-                }
-            }
+            Console.WriteLine(Determinant(matrix));
         }
     }
 }
