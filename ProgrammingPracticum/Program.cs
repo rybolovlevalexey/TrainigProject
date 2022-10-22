1567538806cd7907105713e00a694d4308870319
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-10-22 16:16:10 +0300

    Кубы больше, а квадраты чаще

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -1,65 +1,39 @@
 using System;
 using System.Collections.Generic;
 
-namespace Консольное_приложение
+namespace Задачи_со_Stepika
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            int n = Convert.ToInt32(Console.ReadLine());  // размеры матрицы
-            // создание матрицы из случайных чисел
-            int[,] matrix = new int[n, n];
-            Random rnd = new Random();
-            for (int i = 0; i < n; i += 1)
+            int cnt = 1, kvadr = 1, cube = 1, cur_num = -1;
+            List<int> numbers = new List<int>();
+            int x = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < x; i += 1)
             {
-                for (int j = 0; j < n; j += 1)
+                while (numbers.Contains(kvadr * kvadr) || numbers.Contains(cube * cube * cube))
                 {
-                    int value = rnd.Next(n*n);
-                    matrix[i, j] = value;
-                    Console.Write($"{value} ");
+                    if (numbers.Contains(kvadr * kvadr))
+                        kvadr += 1;
+                    if (numbers.Contains(cube * cube * cube))
+                        cube += 1;
                 }
-                Console.WriteLine();
+                if (kvadr * kvadr < cube * cube * cube)
+                {
+                    Console.WriteLine($"{kvadr}**2 = {kvadr * kvadr}");
+                    cur_num = kvadr * kvadr;
+                    kvadr += 1;
+                } else
+                {
+                    Console.WriteLine($"{cube}**3 = {cube * cube * cube}");
+                    cur_num = cube * cube * cube;
+                    cube += 1;
+                }
+                numbers.Add(cur_num);
             }
-            // вычисление определителя
-
-            static int Determinant(int[,] sp){
-                if (Convert.ToInt32(Math.Pow(sp.Length, 0.5)) == 1)
-                {
-                    return (sp[0, 0]);
-                }
-                if (Convert.ToInt32(Math.Pow(sp.Length, 0.5)) == 2)
-                {
-                    return (sp[0, 0] * sp[1, 1] - sp[0, 1] * sp[1, 0]);
-                }
-                int ans = 0;
-                for (int i = 0; i < Convert.ToInt32(Math.Pow(sp.Length, 0.5)); i += 1)
-                {
-                    int[,] sp1 = new int[Convert.ToInt32(Math.Pow(sp.Length, 0.5)) - 1, Convert.ToInt32(Math.Pow(sp.Length, 0.5)) - 1];
-                    //Console.WriteLine(sp.Length);
-                    for (int j = 1; j < Convert.ToInt32(Math.Pow(sp.Length, 0.5)); j += 1) { 
-                        for (int k = 0; k < Convert.ToInt32(Math.Pow(sp.Length, 0.5)); k += 1)
-                        {
-                            if (i == k)
-                            {
-                                continue;
-                            }
-                            if (i > k)
-                            {
-                                sp1[j - 1, k] = sp[j, k];
-                            } else
-                            {
-                                sp1[j - 1, k - 1] = sp[j, k];
-                            }
-                            
-                        }
-                    }
-                    ans += Convert.ToInt32(Math.Pow((-1), i)) * sp[0, i] * Determinant(sp1);
-                }
-                return ans;
-            }
-
-            Console.WriteLine(Determinant(matrix));
+            Console.WriteLine($"Ответ: {cur_num}");
         }
     }
 }
+
