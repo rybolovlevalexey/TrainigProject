88c5f471777216a1e5ac9aea304d073e1b860523
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2022-11-23 19:40:43 +0300

    добавлен поиск по индексу и проверка на палиндром

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -9,6 +9,18 @@ namespace Урок18._11._22
         public List_item head = new List_item("");
         public MyList() { }
         public MyList(string s) { head.Value = s; }
+        public bool Is_Palindrom()
+        {
+            int dl = this.Length();
+            for (int i = 0; i < dl / 2; i += 1)
+            {
+                if (this.ReturnElementByIndex(i).Value != this.ReturnElementByIndex((i + 1) * (-1)).Value)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
         public int Length()
         {
             if (head.Value == "" && head.Next == null)
@@ -24,6 +36,48 @@ namespace Урок18._11._22
             }
             return dlina;
         }
+
+        // индексация: слева направо от 0, справа налево от -1
+        public List_item ReturnElementByIndex(int ind)
+        {
+            List_item cur = head;
+            if (ind < 0)
+            {
+                int i = 1;
+                ind = Math.Abs(ind);
+                while (cur.Next != null)
+                {
+                    if (i + ind - 1 == this.Length())
+                    {
+                        return cur;
+                    }
+                    cur = cur.Next;
+                    i += 1;
+                }
+                if (i - 1 + ind == this.Length())
+                {
+                    return cur;
+                }
+            } else
+            {
+                int i = 0;
+                while (cur.Next != null)
+                {
+                    if (i == ind)
+                    {
+                        return cur;
+                    }
+                    cur = cur.Next;
+                    i += 1;
+                }
+                if (i == ind)
+                {
+                    return cur;
+                }
+            }
+            return new List_item("error");
+        }
+
         public void Element_IndFromEnd(int ind)
         {
             int i = 1;
