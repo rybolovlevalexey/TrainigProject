f7e6c23e2c99df484855eadfa3c364d9252391e6
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2023-02-21 09:56:55 +0300

    добавлены методы pop, front, size, clear, exit

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -30,6 +30,56 @@ namespace ConsoleApp1
                     cur = cur.Next;
                 cur.Next = new QueueElem(elem);
             }
+            Console.WriteLine("ok");
+        }
+
+        public void pop()
+        {
+            if (Head is null)
+            {
+                Console.WriteLine("error");
+            }
+            else
+            {
+                int value = Head.Value;
+                Head = Head.Next;
+                Console.WriteLine(value);
+            }
+        }
+        public void front()
+        {
+            if (Head is null)
+                Console.WriteLine("error");
+            else
+                Console.WriteLine(Head.Value);
+        }
+
+        public void size()
+        {
+            QueueElem cur = Head;
+            int length = 0;
+            if (cur is null)
+            {
+                length = 0;
+            } else
+            {
+                while (!(cur is null))
+                {
+                    length += 1;
+                    cur = cur.Next;
+                }
+            }
+            Console.WriteLine(length);
+        }
+
+        public void clear()
+        {
+            Head = null;
+            Console.WriteLine("ok");
+        }
+        public void exit()
+        {
+            Console.WriteLine("bye");
         }
     }
     class Program
