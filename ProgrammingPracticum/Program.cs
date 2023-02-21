cb886fdfdd6ee856d86cbca87107412ef6a6bbfb
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2023-02-21 21:10:36 +0300

    Арифметика с приоритетом и скобками

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -1,73 +1,157 @@
 using System;
+using System.Collections.Generic;
 
-namespace ConsoleApp1
+namespace Арифметика_с_приоритетом_действий
 {
     class Program
     {
-        class Answer
-        {
-            public int dlina = -1;
-            public string stroka = "";
-        }
         static void Main(string[] args)
         {
-            string alphabet = "zxcvbnmlkjhgfdsaqwertyuiop";
-            int k = Convert.ToInt32(Console.ReadLine());
             string st = Console.ReadLine();
-            Answer ans = new Answer();
+            List<char> stack = new List<char>();
+            string number = null;
+            string result = "";
 
-            foreach (var letter in alphabet)
+            foreach (var elem in st)
             {
-                int end = 0;
-                int cnt = k;
-                for (int beg = 0; beg < st.Length; beg += 1)
+                if (elem == ' ')
+                    continue;
+                bool is_number = false;
+                foreach (var numb in "1234567890")
                 {
-                    if (st[beg] != letter && cnt > 0)
-                        cnt -= 1;
-                    if (beg != 0 && st[beg - 1] != letter)
-                        cnt += 1;
-
-                    while (end < st.Length)
+                    if (numb == elem)
+                        is_number = true;
+                }
+                if (is_number)
+                    number += elem;
+                else
+                {
+                    if (!(number is null))
                     {
-                        if (end > beg)
-                        {
-                            if (st[end] == letter && cnt == 0)
+                        result += number;
+                        result += " ";
+                        number = null;
+                    }
+                    switch (elem)
+                    {
+                        case '(':
+                            stack.Add(elem);
+                            break;
+                        case '+':
+                            if (stack.Count > 0)
                             {
-                                int dl = end - beg + 1;
-                                if (ans.dlina == -1 || dl > ans.dlina)
+                                stack.Reverse();
+                                while (stack[0] == '*' || stack[0] == '+' || stack[0] == '-')
                                 {
-                                    ans.dlina = dl;
-                                    ans.stroka = st[beg..(end + 1)];
+                                    result += stack[0];
+                                    result += " ";
+                                    stack.RemoveAt(0);
                                 }
+                                stack.Reverse();
                             }
-                            if (st[end] != letter && cnt == 0)
-                                break;
-                            if (st[end] != letter && cnt > 0)
+                            stack.Add(elem);
+                            break;
+                        case '-':
+                            if (stack.Count > 0)
                             {
-                                cnt -= 1;
-                                int dl = end - beg + 1;
-                                if (ans.dlina == 0 || dl > ans.dlina)
+                                stack.Reverse();
+                                while (stack[0] == '*' || stack[0] == '+' || stack[0] == '-')
                                 {
-                                    ans.dlina = dl;
-                                    ans.stroka = st[beg..(end + 1)];
+                                    result += stack[0];
+                                    result += " ";
+                                    stack.RemoveAt(0);
                                 }
+                                stack.Reverse();
                             }
-                            if (st[end] == letter && cnt > 0)
+                            stack.Add(elem);
+                            break;
+                        case '*':
+                            stack.Reverse();
+                            while (stack[0] == '*')
                             {
-                                int dl = end - beg + 1;
-                                if (ans.dlina == 0 || dl > ans.dlina)
-                                {
-                                    ans.dlina = dl;
-                                    ans.stroka = st[beg..(end + 1)];
-                                }
+                                result += stack[0];
+                                result += " ";
+                                stack.RemoveAt(0);
                             }
-                        }
-                        end += 1;
-
+                            stack.Reverse();
+                            stack.Add(elem);
+                            break;
+                        case ')':
+                            int delete_cnt = 0;
+                            stack.Reverse();
+                            foreach (var deyst in stack)
+                            {
+                                if (deyst == '(')
+                                    break;
+                                result += deyst;
+                                result += " ";
+                                delete_cnt += 1;
+                            }
+                            for (int i = 0; i <= delete_cnt; i += 1)
+                                stack.RemoveAt(0);
+                            stack.Reverse();
+                            break;
                     }
                 }
             }
-            Console.WriteLine(ans.dlina);
+            if (!(number is null))
+            {
+                result += number;
+                result += " ";
+            }
+            if (stack.Count > 0)
+            {
+                stack.Reverse();
+                foreach (var deyst in stack)
+                {
+                    result += deyst;
+                    result += " ";
+                }
+            }
+            result = result[0..(result.Length - 1)];
+            Console.WriteLine($"Постфиксная запись: {result}");
+
+            string[] primer = result.Split();
+            List<int> numbers_stack = new List<int>();
+            foreach (string elem in primer)
+            {
+                if (elem == " ")
+                    continue;
+                bool flag_is_number = false;
+                foreach (var numbs in "1234567890")
+                {
+                    
+                    if (elem[0] == numbs)
+                        flag_is_number = true;
+                }
+                if (flag_is_number)
+                {
+                    numbers_stack.Add(Convert.ToInt32(elem));
+                } else
+                {
+                    int first = numbers_stack[numbers_stack.Count - 2];
+                    int second = numbers_stack[numbers_stack.Count - 1];
+                    numbers_stack.RemoveAt(numbers_stack.Count - 1);
+                    numbers_stack.RemoveAt(numbers_stack.Count - 1);
+                    int res = 0;
+                    switch (elem)
+                    {
+                        case "+":
+                            res = first + second;
+                            numbers_stack.Add(res);
+                            break;
+                        case "-":
+                            res = first - second;
+                            numbers_stack.Add(res);
+                            break;
+                        case "*":
+                            res = first * second;
+                            numbers_stack.Add(res);
+                            break;
+                    }
+                }
+            }
+            Console.WriteLine($"Ответ: {numbers_stack[0]}");
         }
     }
 }
