970baac44c864a69af4ad97e8fbc9188f13bb3e7
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2023-02-21 09:50:42 +0300

    создан класс QueueElem и метод push

--- ProgrammingPracticum/Program.cs
+++ ProgrammingPracticum/Program.cs
@@ -2,40 +2,41 @@ using System;
 
 namespace ConsoleApp1
 {
+    public class QueueElem
+    {
+        public int Value;
+        public QueueElem Next = null;
+        public QueueElem(int val)
+        {
+            Value = val;
+        }
+    }
+
+    class Queue
+    {
+        public QueueElem Head;
+        public Queue()
+        {
+            Head = null;
+        }
+        public void push(int elem)
+        {
+            if (Head is null)
+                Head = new QueueElem(elem);
+            else
+            {
+                QueueElem cur = Head;
+                while (!(cur.Next is null))
+                    cur = cur.Next;
+                cur.Next = new QueueElem(elem);
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            string[] st = Console.ReadLine().Split();
-            int n = Convert.ToInt32(st[0]), m = Convert.ToInt32(st[1]), k = Convert.ToInt32(st[2]);
-            int[,] matrix = new int[n, m];
-            for (int i = 0; i < n; i += 1)
-            {
-                string[] line = Console.ReadLine().Split();
-                for (int j = 0; j < m; j += 1)
-                {
-                    matrix[i, j] = Convert.ToInt32(line[j]);
-                }
-            }
-            for (int i = 0; i < k; i++)
-            {
-                int summa = 0;
-                string[] coords = Console.ReadLine().Split();
-                int x1 = Convert.ToInt32(coords[0]) - 1, y1 = Convert.ToInt32(coords[1]) - 1;
-                int x2 = Convert.ToInt32(coords[2]) - 1, y2 = Convert.ToInt32(coords[3]) - 1;
-                int x = x1;
-                while (y1 <= y2)
-                {
-                    while (x1 <= x2)
-                    {
-                        summa += matrix[x1, y1];
-                        x1 += 1;
-                    }
-                    y1 += 1;
-                    x1 = x;
-                }
-                Console.WriteLine(summa);
-            }
+            
         }
     }
 }
