09a44a132e45844b8260745a0b93c94bcc0c1114
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2023-03-01 11:15:25 +0300

    написаны первые 6 тестов

---
+++ ProgrammingPracticum/Testing LinkedLists.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace дз_по_тетсированию_Двусвязных_списков
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string res = Test1();
+        }
+
+        static public string Test1() // for class MyList
+        {
+            string ans = "";
+            Stopwatch spwch = new Stopwatch();
+            spwch.Start();
+            string[] words = { "the", "fox", "jumps", "over", "the", "dog" };
+            MyList ml = new MyList(words);
+            Console.WriteLine("The linked list values:");
+            ml.Prints();
+            
+            ml.AddToTop("today");
+            Console.WriteLine("\nTest 1: Add 'today' to beginning of the list:");
+            ml.Prints();
+
+            string first = ml.head.Value;
+            ml.head = ml.head.Next;
+            ml.Append(first);
+            Console.WriteLine("\nTest 2: Move first node to be last node:");
+            ml.Prints();
+
+            ml.tail.Value = "yesterday";
+            Console.WriteLine("\nTest 3: Change the last node to 'yesterday':");
+            ml.Prints();
+
+            List_item cur = new List_item(ml.tail.Value);
+            ml.tail = ml.tail.Previous;
+            ml.tail.Next = null;
+            cur.Next = ml.head;
+            ml.head = cur;
+            ml.head.Next.Previous = ml.head;
+            Console.WriteLine("\nTest 4: Move last node to be first node:");
+            ml.Prints();
+
+            ml.head = ml.head.Next;
+            ml.head.Previous = null;
+            int ind = ml.Length() - 1;
+            cur = ml.tail;
+            while (cur != null)
+            {
+                if (cur.Value == "the")
+                    break;
+                cur = cur.Previous;
+                ind -= 1;
+            }
+            Console.WriteLine("\nTest 5: Indicate last occurence of 'the':");
+            ml.PrintsEndicated(ind);
+
+            cur = ml.head;
+            while (cur != null)
+            {
+                if (cur.Value == "the")
+                {
+                    List_item lazy_ = new List_item("lazy");
+                    List_item old_ = new List_item("old");
+                    List_item after_cur = cur.Next;
+                    
+                    cur.Next = lazy_;
+                    lazy_.Next = old_;
+                    old_.Next = after_cur;
+
+                    after_cur.Previous = old_;
+                    old_.Previous = lazy_;
+                    lazy_.Previous = cur;
+
+                    if (after_cur is null)
+                        ml.tail = lazy_;
+                }
+            }
+            Console.WriteLine("\nTest 6: Add 'lazy' and 'old' after 'the':");
+            ml.Prints();
+
+            spwch.Stop();
+            return ans;
+        }
+    }
+}
