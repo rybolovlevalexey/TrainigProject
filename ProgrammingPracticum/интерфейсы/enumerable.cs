9ad83f5a5d8120258fab52641a6da292a1ee6c64
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2023-04-04 23:05:07 +0300

    Create enumerable.cs

---
+++ ProgrammingPracticum/интерфейсы/enumerable.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace дз_по_тетсированию_Двусвязных_списков
+{
+    interface IEnumerable
+    {
+        IEnumerator GetEnumerator()
+        {
+            return;
+        }
+    }
+}
