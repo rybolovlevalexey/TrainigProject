18eb71f6a34157afd6db6a7f92eefefcd4308311
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2023-04-10 23:11:23 +0300

    Update Монополия обработчики.cs

--- Монополия обработчики.cs
+++ Монополия обработчики.cs
@@ -12,6 +12,9 @@ namespace Монополия___имитация_консоли
 {
     public partial class Form1 : Form
     {
+        Field main_field = new Field();
+        Player bot1 = new Player();
+        Player bot2 = new Player();
         public Form1()
         {
             InitializeComponent();
@@ -26,8 +29,46 @@ namespace Монополия___имитация_консоли
                 textBox2.ReadOnly = true;
                 textBox1.Text = "2000";
                 textBox2.Text = "2000";
+                //Field main_field = new Field();
             } else
             {
+                int[] result = bot1.move();
+                switch(result[0])
+                {
+                    case -1:
+                        richTextBox1.Text = $"У первого игрока три раза выпали одинаковые числа на кубиках\n" +
+                            richTextBox1.Text;
+                        break;
+                    case -2:
+                        richTextBox1.Text = $"Первый игрок должен пропустить этот ход\n" +
+                            richTextBox1.Text;
+                        bot1.flag_can_move = true;
+                        break;
+                    default:
+                        if (result[2] == 0)
+                            richTextBox1.Text = $"Первый игрок бросает кубики и у него выпадает {result[0]}\n" +
+                                richTextBox1.Text;
+                        else
+                            richTextBox1.Text = $"Первый игрок бросает кубики и у него выпадает {result[0]}\n" + 
+                                "Он начал новый круг поэтому получает дополнительно 100 единиц валюты" +
+                                richTextBox1.Text;
+                        string place = main_field.give_cell_name(result[1]);
+                        if (!main_field.not_for_sale.Contains(place))
+                        {
+                            if (!main_field.has_bought.Contains(place))
+                            {
+                                main_field.has_bought.Add(place);
+                                bot1.buy_new_cell(place, result[1] * 10);
+                            } else
+                            {
+                                bot1.give_money();
+                                bot2.plus_money();
+                            }
+                            
+                        }
+                        break;
+                }
+                
 
             }
         }
@@ -57,6 +98,11 @@ namespace Монополия___имитация_консоли
 
         }  // деньги первого
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
         {
 
         }
