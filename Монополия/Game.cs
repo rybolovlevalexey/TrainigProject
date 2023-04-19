62cd86c48461fccfb2e9253b56a35f9f6d108cf5
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2023-04-19 09:27:58 +0300

    добавлен инициализатор и наполнение лексикона

--- Монополия/Game.cs
+++ Монополия/Game.cs
@@ -1,1 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace Монополия___имитация_консоли
+{
+    class Game
+    {
+        public Game()
+        {
+            Field main_field = new Field();
+            Dictionary<int, Player> players = new Dictionary<int, Player>();
+            players[1] = new Player();
+            players[2] = new Player();
+        }
+
+        public List<string> make_lexicon(int bot_number)
+        {
+            List<string> lexicon = new List<string>();
+            switch (bot_number)  // наполнение лексикона
+            {
+                case 1:
+                    lexicon.Add("первый");
+                    lexicon.Add("первого");
+                    lexicon.Add("первому");
+                    break;
+                case 2:
+                    lexicon.Add("второй");
+                    lexicon.Add("второго");
+                    lexicon.Add("второму");
+                    break;
+                default:
+                    lexicon.Add("lexicon error");
+                    lexicon.Add("lexicon error");
+                    lexicon.Add("lexicon error");
+                    break;
+            }
+            return lexicon;
+        }
+        public void game_iteration()
+        {
+
+        }
+    }
+}
