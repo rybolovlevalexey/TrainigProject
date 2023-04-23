8371906124c6dcddbeaf4cba260cd066b8bdbf7e
Рыболовлев Алексей Александрович <49989260+rybolovlevalexey@users.noreply.github.com>
2023-04-23 19:53:24 +0300

    Update MyEnumerator.cs

--- MyLinkedList/MyEnumerator.cs
+++ MyLinkedList/MyEnumerator.cs
@@ -7,32 +7,47 @@ namespace дз_по_тетсированию_Двусвязных_списков
 {
     class MyEnumerator<T> : IEnumerator<T>
     {
-        public MyList<T> mylist;
-
         public List_item<T> current;
+        public List_item<T> Head_;
+        private bool start_flag = true;
+
+        public MyEnumerator(List_item<T> Head)
+        {
+            current = null;
+            Head_ = Head;
+        }
         public T Current
         {
             get { return current.Value; }
         }
 
-        object IEnumerator.Current => throw new NotImplementedException();
+        object IEnumerator.Current { get { return current; } }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            
         }
 
         public bool MoveNext()
         {
-            current = current.Next;
-            if (current != null)
+            if (start_flag)
+            {
+                start_flag = false;
+                current = Head_;
+                if (current.Next == null)
+                    return false;
                 return true;
-            return false;
+            }
+            if (current.Next == null)
+                return false;
+            current = current.Next;
+            return true;
         }
 
         public void Reset()
         {
-            current = mylist.head;
+            current = null;
+            start_flag = true;
         }
     }
 }
